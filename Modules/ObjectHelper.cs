e88ad20d252d73042fc9e7d647f1f3615741a245
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2026-08-14 15:59:25 -0400

    better persistent finder

--- Modules/ObjectHelper.cs
+++ Modules/ObjectHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using TeamCherry.Localization;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace BasicItemSync.Modules;
@@ -8,27 +10,14 @@ internal static class ObjectHelper
 {
     public static PersistentItem<T>? FindPersistent<T>(string sceneName, string id) where T : IEquatable<T>
     {
-        var currentScene = SceneManager.GetActiveScene();
-        var objs = currentScene.GetRootGameObjects();
-
-        foreach (var obj in objs)
-        {
-            var components = obj.GetComponentsInChildren<PersistentItem<T>>();
-            foreach (var item in components)
-            {
-                if (item.ItemData.ID == id && item.ItemData.SceneName == sceneName)
-                {
-                    return item;
-                }
-            }
-        }
-
-        return null;
+        var scene = SceneManager.GetActiveScene();
+        return Resources.FindObjectsOfTypeAll<PersistentItem<T>>()
+            .FirstOrDefault(p => p.ItemData.ID == id && p.ItemData.SceneName == sceneName && p.gameObject.scene == scene);
     }
 
     extension(Language)
     {
-        public static string Get(LocalisedString str)
+        public static string GetLocal(LocalisedString str)
         {
             return Language.Get(str.Key, str.Sheet);
         }
