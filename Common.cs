f0375fbe3ceb638c0b729bdc5c8b3ca85e0cac38
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2026-08-15 20:00:57 -0400

    remove outdated function

--- Common.cs
+++ Common.cs
@@ -7,43 +7,6 @@ internal class Common
     internal const string AddonName = "BasicItemSync";
     internal const string AddonVersion = "0.1.1";
     internal const int AddonApiVersion = 1;
-
-
-    public static bool FlagAllowed(SyncServerSettings settings, FlagType flag)
-    {
-        if (ServerAddon.Settings.KillSwitch) return false;
-
-        return flag switch
-        {
-            FlagType.Ability => settings.SyncAbilities,
-            FlagType.Map => settings.SyncMaps,
-            FlagType.Pin => settings.SyncPins,
-            FlagType.Bellshrine => settings.SyncProgression,
-            FlagType.Boss => settings.SyncBattles,
-            FlagType.Arena => settings.SyncBattles,
-            FlagType.Progression => settings.SyncProgression,
-            FlagType.Flea => settings.SyncCollectables,
-            FlagType.Collectable => settings.SyncCollectables,
-            FlagType.Bellway => settings.SyncTransit,
-            FlagType.Ventrica => settings.SyncTransit,
-            FlagType.Currency => settings.SyncCurrency,
-
-            FlagType.Mask => settings.SyncUpgrades,
-            FlagType.Spool => settings.SyncUpgrades,
-            FlagType.Pouch => settings.SyncUpgrades,
-            FlagType.CraftingKit => settings.SyncUpgrades,
-            FlagType.Quest => settings.SyncQuests,
-            FlagType.Tool => settings.SyncTools,
-            FlagType.Crest => settings.SyncTools,
-            FlagType.SilkHeart => settings.SyncUpgrades,
-            FlagType.Needle => settings.SyncUpgrades,
-            FlagType.Bench => settings.SyncProgression,
-            FlagType.Shortcut => true,
-            FlagType.QuestItem => settings.SyncQuestItems,
-            FlagType.DoNotSync => false,
-            _ => false,
-        };
-    }
 }
 
 internal enum FlagType
